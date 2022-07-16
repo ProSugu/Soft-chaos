9dd6bd64e7c7f42e0133d9c13be2065c2b5a2b9d
Praveen Arya <praveen.kumar@codebrewinnovations.com>
2022-07-16 13:53:58 +0530

    feedback changes

--- src/app/tour/tour.component.ts
+++ src/app/tour/tour.component.ts
@@ -15,7 +15,85 @@ export class TourComponent implements OnInit {
     vertical:true,
     speed: 1000
   };
+  
+  public slideConfigTes = {
+    'slidesToShow': 3,
+    'slidesToScroll': 3,
+    'dots': false,
+    'arrows': true,
+    'infinite': true,
+    vertical:false,
+    speed: 1000,
+    responsive: [
+      {
+        breakpoint: 920,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
 
+        }
+      },
+      {
+        breakpoint: 556,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ],
+    prevArrow: "<button type='button' matRipple class='slick-prev pull-right'><img src='assets/images/left-arrow.png'></button>",
+    nextArrow: "<button type='button' matRipple class='slick-next pull-right'><img src='assets/images/right-arrow.png'></button>",
+  };
+
+
+  public slideConfigCar = {
+    'slidesToShow': 1,
+    'slidesToScroll': 1,
+    'dots': false,
+    'arrows': true,
+    'infinite': true,
+    vertical:false,
+    speed: 1000,
+    responsive: [
+      {
+        breakpoint: 920,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+
+        }
+      },
+      {
+        breakpoint: 556,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ],
+    prevArrow: "<button type='button' matRipple class='slick-prev pull-right'><img src='assets/images/left-arrow.png'></button>",
+    nextArrow: "<button type='button' matRipple class='slick-next pull-right'><img src='assets/images/right-arrow.png'></button>",
+  };
   constructor() { }
 
   ngOnInit(): void {
