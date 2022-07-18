84549574b0aea8b2522dfd4a0a206132ac26a8a8
sug <46050747+ProSugu@users.noreply.github.com>
2022-07-18 12:11:31 +0530

    changes

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./booking.component.scss']
 })
 export class BookingComponent implements OnInit {
-
+  public step = 1;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  next():void {
+    this.step++;
+  }
+  prev():void {
+    this.step--;
+  }
 }
