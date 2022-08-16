a534232e64b3db5edb63565cdacd95f55d5e53ab
sug <46050747+ProSugu@users.noreply.github.com>
2022-08-16 18:54:54 +0530

    changes

---
+++ src/app/dashboard/dashboard-section.constants.ts
@@ -0,0 +1,38 @@
+export const OBJECTIVES_SLIDES = [
+    {
+        bannerImage: 'assets/images/Objectives/1st.png',
+        tagName: 'Understand',
+        sectionName: 'Empathize',
+        infoText: 'Conduct researches, interviews and shadowing customer for better understanding without judging.'
+    },
+    {
+        bannerImage: 'assets/images/Objectives/2nd.png',
+        tagName: 'Understand',
+        sectionName: 'Define',
+        infoText: 'Identify and pinpoint the list of problems in hand along with objectives, personas, and challenges.'
+    },
+    {
+        bannerImage: 'assets/images/Objectives/3rd.png',
+        tagName: 'Explore',
+        sectionName: 'Ideate',
+        infoText: 'Generate a list of ideas. Share ideas, discuss, segment, and prioritize ideas.'
+    },
+    {
+        bannerImage: 'assets/images/Objectives/4rth.png',
+        tagName: 'Explore',
+        sectionName: 'Prototype Build',
+        infoText: 'Build representations for a range of ideas. Create a simple storyboard and don’t be afraid to iterate or to fail fast.'
+    },
+    {
+        bannerImage: 'assets/images/Objectives/5th.png',
+        tagName: 'Materialize',
+        sectionName: 'Test',
+        infoText: 'Understand impediments, seek user feedback and use it to improvise your user’s experience.'
+    },
+    {
+        bannerImage: 'assets/images/Objectives/6th.png',
+        tagName: 'Materialize',
+        sectionName: 'Implement',
+        infoText: 'Put your vision into action, recognize the synergy and achieve project success.'
+    }
+]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,9 @@
-import {Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ApiService } from '../core/service/api.service';
 import { Router } from '@angular/router';
+import { OBJECTIVES_SLIDES } from './dashboard-section.constants';
+import { DeviceService } from '../core/service/device.service';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,7 +11,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
-  homeSlider = ['assets/images/s1.png','assets/images/s2.png','assets/images/s3.png','assets/images/s1.png','assets/images/s2.png'];
+  homeSlider = ['assets/images/s1.png', 'assets/images/s2.png', 'assets/images/s3.png', 'assets/images/s1.png', 'assets/images/s2.png'];
   pageSlideAnimationActive = false;
   requestingServerForBannerText = false;
   requestingServerForLocations = false;
@@ -19,19 +22,16 @@ export class DashboardComponent {
   leftSlideConfig = {
     slidesToShow: 2,
     slidesToScroll: 1,
-    items :2,
+    items: 2,
     centerMode: true,
     vertical: true,
-    margin:10,
-    arrows: true,
+    dots: false,
+    nav: false,
+    infinite: false,
+    margin: 10,
     verticalSwiping: true,
     centerPadding: '75px',
-    dots: true,
-    infinite: true,
-    speed: 300,
-    variableWidth: true,
-    prevArrow:'<i class=" fas fa-angle-right clr-gry"></i>',
-    nextArrow:'<i class="fas fa-angle-left clr-gry"></i>',
+    navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
     responsive: [
       {
         breakpoint: 1024,
@@ -72,34 +72,10 @@ export class DashboardComponent {
     'slidesToShow': 1,
     'slidesToScroll': 1,
     'dots': false,
-    'arrows': true,
+    'arrows': false,
     'infinite': false,
-    responsive: [
-      {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 1.5,
-        }
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow:1,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-        }
-      },
-      {
-        breakpoint: 360,
-        settings: {
-          slidesToShow: 1,
-        }
-      }
-    ],
+    verticalSwiping: true,
+    vertical: true
   };
   slideConfigSec = {
     slidesToShow: 3,
@@ -186,19 +162,64 @@ export class DashboardComponent {
   private verticalSliderInitRef: any;
   @ViewChild('slickModal1', { static: true }) verticalSliderDomRef: ElementRef | any;
   allLocations: any = [];
+  public readonly OBJECTIVE_SLIDES = OBJECTIVES_SLIDES;
+  public gridGalleryImageList = [];
 
   constructor(
     private apiService: ApiService,
-    private router:Router,) {
+    public deviceService: DeviceService,
+    private router: Router,) {
     this.loadDashboardData();
   }
 
-  public loadDashboardData() : void {
+  debouncing(fn: any, d: number) {
+    let timer: any;
+    const scope = this;
+    return function () {
+      let context = scope;
+      let args = arguments;
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        fn.apply(context, args);
+      }, d);
+    }
+  }
+
+  afterObjectiveCarouselChange(event: any, slaveCarousel: any) {
+    this.VerticalSlideNumber = event.currentSlide + 1;
+  }
+
+  onVerticalSliderInit(e: any) {
+    this.verticalSliderInitRef = e.event.target;
+    // this.verticalSliderInitRef.addEventListener('wheel', this.debouncing(this.onVerticalSliderScroll, 10), { passive: false });
+    this.verticalSliderInitRef.addEventListener('wheel', this.onVerticalSliderScroll, { passive: false });
+  }
+
+  onVerticalSliderScroll = (event: any): void => {
+    if (event.deltaY < 0) {
+      if (this.VerticalSlideNumber == 1) {
+        return;
+      }
+      event.preventDefault();
+      event.stopPropagation();
+      this.verticalSliderDomRef.slickPrev();
+    } else {
+      if (this.VerticalSlideNumber == this.OBJECTIVE_SLIDES.length) {
+        return;
+      }
+      event.preventDefault();
+      event.stopPropagation();
+      this.verticalSliderDomRef.slickNext();
+    }
+  };
+
+  public loadDashboardData(): void {
     this.requestingServerForBannerText = true
     setTimeout(() => {
       this.apiService.getdashboardData().subscribe((response: any) => {
         this.dashboardData = response;
         this.requestingServerForBannerText = false;
+        this.gridGalleryImageList = response.mainBannerImagesList.slice(0, 10);
       })
     }, 3000)
   }
@@ -207,13 +228,31 @@ export class DashboardComponent {
     carouselRef.slickGoTo(slideIndex);
   }
 
+  trackItem(index:number,item:string){
+    return index;
+  }
+
   afterSlaveCarouselChange(event: any) {
     this.horizontalSlideNumber = event.currentSlide + 1;
   }
 
   public exploreTour(): void {
-    this.router.navigate(['/','tour'])
+    this.router.navigate(['/', 'tour'])
   }
+
+  isSectionInViewport(i: any) {
+    var myElement: any = document.getElementById(`info${i}`);
+    if(myElement) {
+      var bounding = myElement.getBoundingClientRect();
+      if (bounding.top >= 0 && bounding.left >= 0 && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
+        return true
+      } else {
+        return false;
+      }
+    }
+    return false;
+  };
+
 }
 
 
